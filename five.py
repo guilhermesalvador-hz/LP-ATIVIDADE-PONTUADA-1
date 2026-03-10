ac3b1b038d1cfc26df175a06840f1e267be42afb
guilhermesalvador-hz <jorge.salvador@ba.estudante.senai.br>
2026-03-10 15:05:51 -0300

    Add files via upload

---
+++ five.py
@@ -0,0 +1,27 @@
+import os
+
+os.system("cls")
+
+a = int(input("Digite seu numero:"))
+operador = input("Digite o operador(+,-,*,/)")
+b = int(input("Digite seu numero:"))
+
+macth operador:
+    case "+":
+        resultado = a + b
+    case "-":
+        resultado = a - b
+    case "*":
+        resultado = a * b
+    case "/":
+        resultado = a / b
+    case_:
+        print("Opcao invalida.")
+        resultado = 0
+        
+print(f"Resultado: {resultado}")        
+        
+        
+    
+    
+
