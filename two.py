b687108cb348a3788320a95c3b7b5b29f51dd42b
guilhermesalvador-hz <jorge.salvador@ba.estudante.senai.br>
2026-03-10 14:43:27 -0300

    Add files via upload

---
+++ two.py
@@ -0,0 +1,26 @@
+import os
+
+os.system
+
+
+nome = input("Digite seu nome:")
+sexo =  input("Digite seu sexo (M/F):").lower()
+estado_civil = input("Digite o estado civil:").lower()
+
+
+if sexo == "F" and estado_civil == "casada":
+    print tempo_casada = int(input("Digite o tempo de casada em anos:"))
+       
+
+print(" ===== Dados da pessoa =====")
+print("Nome:", nome)
+print("Sexo:", sexo)       
+print("Estado Civil", estado_civil)
+
+if tempo_casada and tempo_casada :
+    print("Tempo de casada:", tempo_casada, "anos")
+    
+    
+       
+       
+       
